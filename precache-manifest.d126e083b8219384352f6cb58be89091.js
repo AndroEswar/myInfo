b10d28f71ca9564f9ae02a2b88289137189e1858
EswaraiahSapram <eswar.rom@gmail.com>
2020-09-23 11:37:05 +0530

    Updates

---
+++ precache-manifest.d126e083b8219384352f6cb58be89091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3af7ddb4d7d1f2e793c479fb22c3f24",
+    "url": "/myInfo/index.html"
+  },
+  {
+    "revision": "aa42db736a1d69adf04c",
+    "url": "/myInfo/static/css/main.d9424428.chunk.css"
+  },
+  {
+    "revision": "10c8cfa3e37433db9283",
+    "url": "/myInfo/static/js/2.b57f97cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myInfo/static/js/2.b57f97cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa42db736a1d69adf04c",
+    "url": "/myInfo/static/js/main.2675b1de.chunk.js"
+  },
+  {
+    "revision": "3ab9cfa6fa74c9df29b8",
+    "url": "/myInfo/static/js/runtime-main.1d2dd1bc.js"
+  }
+]);
